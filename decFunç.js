e750f6a6eea1b296fb5dc7069b377db39e91fc63
Gustavo Rodrigues <gustavogrf2015@gmail.com>
2025-02-26 18:58:57 -0300

    atualizando exercicio biel

--- decFunç.js
+++ decFunç.js
@@ -1,11 +1,12 @@
 const name1 = "Gustavo";
 const age1 = 30;
+const anoNascimento = 1994;
 const temCarteira1 = true;
 let saldo1 = 100;
 
 const name2 = "Gabriel";
 const age2 = 65;
-const temCarteira2 = false;
+let temCarteira2 = false;
 let saldo2 = 200;
 
 console.log(
@@ -15,22 +16,36 @@ console.log(
   `Pessoa 2 : Name = ${name2} , age = ${age2} , Tem carteira = ${temCarteira2}`
 );
 
-function AtualizaSaldo(saldo, valor) {
-  return saldo + valor;
+function atualizaSaldo(saldo, valor) {
+  const resultado = saldo + valor;
+  console.log(`o seu novo saldo é ${resultado}`);
+  return resultado;
 }
 
 if (temCarteira1) {
-  saldo1 = AtualizaSaldo(saldo1, 200);
+  saldo1 = atualizaSaldo(saldo1, 200);
 }
 if (temCarteira2) {
-  saldo2 = AtualizaSaldo(saldo2, 200);
+  saldo2 = atualizaSaldo(saldo2, 200);
 }
 temCarteira2 = true;
-saldo1 = AtualizaSaldo(saldo1, -450);
-saldo2 = AtualizaSaldo(saldo2, -450);
+if (temCarteira2) {
+  saldo2 = atualizaSaldo(saldo2, -100);
+}
+saldo1 = atualizaSaldo(saldo1, -450);
+saldo2 = atualizaSaldo(saldo2, -450);
 if (age1 > 50) {
-  saldo1 = AtualizaSaldo(saldo1 + 1000);
+  saldo1 = atualizaSaldo(saldo1 + 1000);
 }
 if (age2 > 50) {
-  saldo2 = AtualizaSaldo(saldo2 + 1000);
+  saldo2 = atualizaSaldo(saldo2 + 1000);
 }
+
+function calculeIdade(ano) {
+  const REGRA_ANO = 2030;
+  const resultadoIdade = REGRA_ANO - ano;
+  console.log(`a idade da pessoa neste ano será de ${resultadoIdade}`);
+  return resultadoIdade;
+}
+
+calculeIdade(anoNascimento);
