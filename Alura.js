4e7229c864d5bbc8782fc1b3978d7f12f496bcf7
Gustavo Rodrigues <gustavogrf2015@gmail.com>
2025-03-03 23:03:32 -0300

    alura

--- Alura.js
+++ Alura.js
@@ -1,10 +1,24 @@
 alert("boas vindas ao jogo do número secreto");
 let NumeroSecreto = 5;
 console.log(NumeroSecreto);
-let chute = prompt("Escolha um número entre 1 e 10");
+let chute;
+let tentativas = 1;
 
-if (chute == NumeroSecreto) {
-  alert(`Isso ai ! você acertou o número secreto ${NumeroSecreto}`);
-} else {
-  alert(" Você errou !");
+// enquanto chute nao for igual ao número secreto
+while (chute != NumeroSecreto) {
+  chute = prompt("Escolha um número entre 1 e 10");
+  // se chute for igual o número secreto
+  if (chute == NumeroSecreto) {
+    alert(
+      `Isso ai ! você acertou o número secreto ${NumeroSecreto} com ${tentativas} tentativas`
+    );
+  } else {
+    if (chute > NumeroSecreto) {
+      alert(`o número secreto é menor que ${chute}`);
+    } else {
+      alert(`o número secreto é maior que ${chute}`);
+    }
+    // tentativas = tentativas + 1
+    tentativas++;
+  }
 }
